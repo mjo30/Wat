a0f306812cdddd86520c83803c1e6deb035420b6
SsoyeonLee <maylinnlee@gmail.com>
2017-04-11 12:56:02 -0400

    code analysis done

--- app/src/main/java/com/example/danae/watopia/LoginActivityPage.java
+++ app/src/main/java/com/example/danae/watopia/LoginActivityPage.java
@@ -1,7 +1,6 @@
 package com.example.danae.watopia;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,15 +8,12 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivityPage extends AppCompatActivity {
-    Button login;
-    Button cancel;
-    Button regi;
-    EditText user, pass;
-    DataBaseHandler db;
-    Cursor cursor;
+    // --Commented out by Inspection (4/11/2017 1:37 AM):EditText user, pass;
+    private DataBaseHandler db;
+    // --Commented out by Inspection (4/11/2017 1:37 AM):Cursor cursor;
 
-    EditText ed1;
-    EditText ed2;
+    private EditText ed1;
+    private EditText ed2;
 
     private static String userName;
     /**
@@ -34,26 +30,30 @@ public class LoginActivityPage extends AppCompatActivity {
 
         ed1 = (EditText)findViewById(R.id._username);
         ed2 = (EditText)findViewById(R.id._password);
-        login = (Button) findViewById(R.id._login);
-        cancel = (Button) findViewById(R.id.button5);
-        regi = (Button) findViewById(R.id.regBtn);
+        Button login = (Button) findViewById(R.id._login);
+        Button cancel = (Button) findViewById(R.id.button5);
+        Button regi = (Button) findViewById(R.id.regBtn);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                db = new DataBaseHandler(LoginActivityPage.this, null, null, 2);
+                db = new DataBaseHandler(LoginActivityPage.this);
                 userName = ed1.getText().toString();
                 String password = ed2.getText().toString();
                 String storedPw = db.getRegister(userName);
                 String standing = db.getKeyStanding(userName);
                 System.out.println(standing);
                 if(password.equals(storedPw)) {
-                    if (standing.toString().equals("User")) {
-                        startActivity(new Intent(getApplicationContext(),LoggedIn2.class));
-                    } else if (standing.toString().equals("Worker")) {
-                        startActivity(new Intent(getApplicationContext(), workerLoggedIn.class));
-                    } else if (standing.toString().equals("Manager")) {
-                        startActivity(new Intent(getApplicationContext(), managerLoggedIn.class));
+                    switch (standing) {
+                        case "User":
+                            startActivity(new Intent(getApplicationContext(), LoggedIn2.class));
+                            break;
+                        case "Worker":
+                            startActivity(new Intent(getApplicationContext(), workerLoggedIn.class));
+                            break;
+                        case "Manager":
+                            startActivity(new Intent(getApplicationContext(), managerLoggedIn.class));
+                            break;
                     }
                 } else {
                     startActivity(new Intent(getApplicationContext(),ErrorScreen.class));
