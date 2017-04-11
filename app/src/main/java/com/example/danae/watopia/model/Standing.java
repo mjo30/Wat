05c6dd07413930de549abeffaecfa816033ff73a
SsoyeonLee <maylinnlee@gmail.com>
2017-04-11 12:48:47 -0400

    code analysis done

--- app/src/main/java/com/example/danae/watopia/model/Standing.java
+++ app/src/main/java/com/example/danae/watopia/model/Standing.java
@@ -1,18 +1,12 @@
 package com.example.danae.watopia.model;
 import java.io.Serializable;
 
-/**
- * Created by danae on 2/21/2017.
- */
-/**
- * enum to represent various standings(user types)
- */
 public enum Standing implements Serializable{
     USER("User"),
     WORKER("Worker"),
     MANAGER("Manager"),
     ADMIN("Admin");
-    private String standing;
+    private final String standing;
     Standing(String standing) {this.standing = standing;}
     public String toString() { return "" + standing;}
 }
