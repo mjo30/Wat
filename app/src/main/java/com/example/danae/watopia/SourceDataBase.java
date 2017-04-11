5b47b3159b730a7300bd331325bb21bb4dcbb1ac
SsoyeonLee <maylinnlee@gmail.com>
2017-04-11 12:59:10 -0400

    code analysis done

--- app/src/main/java/com/example/danae/watopia/SourceDataBase.java
+++ app/src/main/java/com/example/danae/watopia/SourceDataBase.java
@@ -4,35 +4,33 @@ import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
-import com.example.danae.watopia.model.QualityReport;
 import com.example.danae.watopia.model.WaterReport;
 
 import java.util.List;
 
-/**
- * Created by Minky on 2017-04-02.
- */
-
 public class SourceDataBase implements DataReportSource {
     private SQLiteDatabase database;
-    private SourceDbHelper helper;
+    private final SourceDbHelper helper;
     public SourceDataBase(Context context) {
         helper = new SourceDbHelper(context);
         open();
     }
-    public SourceDataBase() {
-        open();
-    }
+// --Commented out by Inspection START (4/11/2017 1:41 AM):
+//    public SourceDataBase() {
+//        open();
+//    }
+// --Commented out by Inspection STOP (4/11/2017 1:41 AM)
     private void open() throws SQLException {
         database = helper.getWritableDatabase();
     }
-    public void close() {
-        helper.close();
-    }
-    //create(SQLiteDatabase database, String name, String date,String location,String virus, String condition, String contamination) {
+// --Commented out by Inspection START (4/11/2017 1:41 AM):
+//    public void close() {
+//        helper.close();
+//    }
+// create(SQLiteDatabase database, String name, String date,String location,String virus, String condition, String contamination) {
     @Override
     public WaterReport createReport(String name, String date, String latitude, String longitude,
-                             String type, String waterCondition) {
+                                    String type, String waterCondition) {
         return WaterSourceDAO.create(database, name, date,
                 latitude, longitude, type, waterCondition);
     }
@@ -50,13 +48,7 @@ public class SourceDataBase implements DataReportSource {
         return WaterSourceDAO.getAllReports(database);
     }
 
-    @Override
-    public void updateReport(WaterReport report) {
 
-    }
-
-
-    @Override
     public WaterReport findReportByName(String name) {
         return WaterSourceDAO.findByName(database, name);
     }
