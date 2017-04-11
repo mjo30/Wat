d0dde4f01cf2a46d392666ed4d8e308a16d2a23d
SsoyeonLee <maylinnlee@gmail.com>
2017-04-11 12:51:44 -0400

    code analysis done

--- app/src/main/java/com/example/danae/watopia/DataReportSource.java
+++ app/src/main/java/com/example/danae/watopia/DataReportSource.java
@@ -1,20 +1,16 @@
 package com.example.danae.watopia;
 
-import com.example.danae.watopia.model.QualityReport;
 import com.example.danae.watopia.model.WaterReport;
 
 import java.util.List;
 
-/**
- * Created by Minky on 2017-04-02.
- */
 
-public interface DataReportSource {
+interface DataReportSource {
     WaterReport createReport(String name, String date, String latitude, String longitude,
                              String type, String waterCondition);
 
     void deleteReport(WaterReport report);
     List<WaterReport> getAllReports();
-    void updateReport(WaterReport report);
-    WaterReport findReportByName(String name);
+
+    // --Commented out by Inspection (4/11/2017 1:36 AM):WaterReport findReportByName(String name);
 }
