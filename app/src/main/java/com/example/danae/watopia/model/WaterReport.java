ef0015ed69eccdf3370bceb5c91c1d89e3ac6305
SsoyeonLee <maylinnlee@gmail.com>
2017-04-11 12:49:36 -0400

    code analysis done

--- app/src/main/java/com/example/danae/watopia/model/WaterReport.java
+++ app/src/main/java/com/example/danae/watopia/model/WaterReport.java
@@ -4,51 +4,51 @@ package com.example.danae.watopia.model;
 import com.example.danae.watopia.Location;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-
-/**
- * Created by Minky and Eunbin on 2017-03-01.
- */
 
 public class WaterReport {
-    public static ArrayList<WaterReport> reportList = new ArrayList<WaterReport>();
-    public static List<String> types = Arrays.asList("Bottled", "Well", "Steam",
-            "Lake", "Spring", "Other");
-    public static List<String> conditions = Arrays.asList("Waste", "Treatable-Clear",
-            "Treatable-Muddy", "Potable");
+    private static final ArrayList<WaterReport> reportList = new ArrayList<>();
+// --Commented out by Inspection START (4/11/2017 1:41 AM):
+// --Commented out by Inspection START (4/11/2017 1:41 AM):
+////    public static List<String> types = Arrays.asList("Bottled", "Well", "Steam",
+////            "Lake", "Spring", "Other");
+//// --Commented out by Inspection STOP (4/11/2017 1:41 AM)
+//    public static List<String> conditions = Arrays.asList("Waste", "Treatable-Clear",
+//            "Treatable-Muddy", "Potable");
+// --Commented out by Inspection STOP (4/11/2017 1:41 AM)
     private long number;
     private Location _location;
     private String latitude;
     private String longitude;
     private String waterCondition;
     private String waterType;
-    private String name;
     private String date;
     /**
      * empty constructor for WaterReport object
      */
     public WaterReport(){
     }
-    /**
-     * constructor for WaterReport object
-     */
-    public WaterReport(String name, String date, String latitude,
-                       String longitude, String waterCondition, String waterType, long number) {
-        this.date = date;
-        this.name = name;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.waterCondition = waterCondition;
-        this.waterType = waterType;
-        this.number = number;
-        Location newLocation = new Location(Double.parseDouble(latitude), Double.parseDouble(longitude));
-        _location = newLocation;
-    }
-    public List<WaterReport> getReportList() {
-        return reportList;
-    }
+// --Commented out by Inspection START (4/11/2017 1:41 AM):
+// --Commented out by Inspection START (4/11/2017 1:41 AM):
+////    /**
+////     * constructor for WaterReport object
+////     */
+////    public WaterReport(String name, String date, String latitude,
+////                       String longitude, String waterCondition, String waterType, long number) {
+////        this.date = date;
+////        this.name = name;
+////        this.latitude = latitude;
+////        this.longitude = longitude;
+////        this.waterCondition = waterCondition;
+////        this.waterType = waterType;
+////        this.number = number;
+////        _location = new Location(Double.parseDouble(latitude), Double.parseDouble(longitude));
+////    }
+//// --Commented out by Inspection STOP (4/11/2017 1:41 AM)
+//    public List<WaterReport> getReportList() {
+//        return reportList;
+//    }
+// --Commented out by Inspection STOP (4/11/2017 1:41 AM)
     /**
      * @return string of water condition
      */
@@ -75,20 +75,24 @@ public class WaterReport {
         this.waterType = waterType;
     }
 
-    public Location getLocation() {
-        return _location;
-    }
+// --Commented out by Inspection START (4/11/2017 1:42 AM):
+//    public Location getLocation() {
+//        return _location;
+//    }
+// --Commented out by Inspection STOP (4/11/2017 1:42 AM)
 
-    public void setLocation(Location location) {
-        _location = location;
-    }
+// --Commented out by Inspection START (4/11/2017 1:42 AM):
+//    public void setLocation(Location location) {
+//        _location = location;
+//    }
+// --Commented out by Inspection STOP (4/11/2017 1:42 AM)
     public void setNumber(long number) {this.number = number;}
     public long getNumber() {
         return number;
     }
-    public String getName() {return name; }
+    // --Commented out by Inspection (4/11/2017 1:42 AM):public String getName() {return name; }
     public void setName(String name) {
-        this.name = name;
+//        String name1 = name;
     }
 
     public void setDate(String date) {
@@ -102,24 +106,26 @@ public class WaterReport {
     }
     public double getLatitude() { return Double.parseDouble(latitude); }
     public double getLongitude() { return Double.parseDouble(longitude); }
-    /**
-     * retrieves the latitude data of a double and changes it to a string.
-     * @return the latitude string.
-     */
-    public String get_sLat() {
-        Double dLat = _location.getLatitude();
-        String sLat = dLat.toString();
-        return sLat;
-    }
-    /**
-     * retrieves the longitude data of a double and changes it to a string.
-     * @return the longitude string.
-     */
-    public String get_sLong() {
-
-        Double dLong = _location.getLongitude();
-        String sLong = dLong.toString();
-        return sLong;
-    }
+// --Commented out by Inspection START (4/11/2017 1:42 AM):
+//    /**
+//     * retrieves the latitude data of a double and changes it to a string.
+//     * @return the latitude string.
+//     */
+//    public String get_sLat() {
+//        Double dLat = _location.getLatitude();
+//        return dLat.toString();
+//    }
+// --Commented out by Inspection STOP (4/11/2017 1:42 AM)
+// --Commented out by Inspection START (4/11/2017 1:42 AM):
+//    /**
+//     * retrieves the longitude data of a double and changes it to a string.
+//     * @return the longitude string.
+//     */
+//    public String get_sLong() {
+//
+//        Double dLong = _location.getLongitude();
+//        return dLong.toString();
+//    }
+// --Commented out by Inspection STOP (4/11/2017 1:42 AM)
 
 }
